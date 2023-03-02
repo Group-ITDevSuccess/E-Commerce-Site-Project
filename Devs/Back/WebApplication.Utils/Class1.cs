1085cff8493fccb4ed0cb7bbf26c06946f67310e
ITDevSuccess <raharisontsidiany@gmail.com>
2023-03-02 17:21:31 +0300

    Envoi de Theme.docx

---
+++ Devs/Back/WebApplication.Utils/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApplication.Utils
+{
+    public class Class1
+    {
+    }
+}
