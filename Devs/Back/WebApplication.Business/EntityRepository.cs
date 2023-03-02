c309eed3652cbaeac41a58883ef49eb24b6d533b
ITDevSuccess <raharisontsidiany@gmail.com>
2023-03-03 00:14:17 +0300

    Ajout de quelque table dans le MCD et Mise en place de creation de entité utilisateur cote backend ainsi que les fonctionnalité CRUD de Users

---
+++ Devs/Back/WebApplication.Business/EntityRepository.cs
@@ -0,0 +1,103 @@
+﻿using NHibernate;
+using NHibernate.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApplication.Entity;
+
+namespace WebApplication.Business
+{
+    public class EntityRepository<T> where T : Entity.Entity
+    {
+        public virtual async Task<T> GetById(Guid Id)
+        {
+            using(ISession session = NHibernateHelper.GetSessionFactory().OpenSession())
+            {
+                return await session.GetAsync<T>(Id);
+            }
+        }
+
+        public virtual async Task<List<T>> GetAll(int skip=0, int take = -1)
+        {
+            using (ISession session = NHibernateHelper.GetSessionFactory().OpenSession())
+            {
+                IQueryable<T> query = session.Query<T>();
+
+                if (skip > 0)
+                {
+                    query = query.Skip(skip);
+                }
+                if (take > -1)
+                {
+                    query = query.Take(take);
+                }
+
+                var a = await query.ToListAsync<T>();
+                return a;
+            }
+        }
+
+        public virtual void SaveOrUpdate(T entity)
+        {
+            using (ISession session = NHibernateHelper.GetSessionFactory().OpenSession())
+            {
+                using(ITransaction transaction = session.BeginTransaction())
+                {
+                    try
+                    {
+                        session.SaveOrUpdate(entity);
+                        transaction.Commit();
+                    }
+                    catch (Exception e)
+                    {
+                        transaction.Rollback();
+                        throw e;
+                    }
+                }
+            }
+        }
+
+        public virtual async void SaveOrUpdateAsynk(T entity)
+        {
+            using (ISession session = NHibernateHelper.GetSessionFactory().OpenSession())
+            {
+                using (ITransaction transaction = session.BeginTransaction())
+                {
+                    try
+                    {
+                        await session.SaveOrUpdateAsync(entity);
+                        transaction.Commit();
+                    }
+                    catch (Exception e)
+                    {
+                        transaction.Rollback();
+                        throw e;
+                    }
+                }
+            }
+        }
+
+        public virtual async Task DeleteById(Guid Id)
+        {
+            using (ISession session = NHibernateHelper.GetSessionFactory().OpenSession())
+            {
+                using (ITransaction transaction = session.BeginTransaction())
+                {
+                    try
+                    {
+                        T entity = session.Get<T>(Id);
+                        await session.DeleteAsync(entity);
+                        transaction.Commit();
+                    }
+                    catch (Exception e)
+                    {
+                        transaction.Rollback();
+                        throw e;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Devs/Back/WebApplication.Entity/Users.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApplication.Entity
+{
+    public class Users : Entity
+    {
+        public virtual string FirstNameUser { get; set; }
+        public virtual string LastNameUser { get; set; }
+        public virtual DateTime BirthDayUser { get; set; }
+        public virtual bool GenreUser { get; set; }
+    }
+}
