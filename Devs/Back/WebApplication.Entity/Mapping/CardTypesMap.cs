5e3fce67e4d7f0b253ffb0f7deb3761dcb55b634
ITDevSuccess <raharisontsidiany@gmail.com>
2023-03-13 20:03:00 +0300

    Save Fin de rectification de CardTypes

---
+++ Devs/Back/WebApplication.Entity/Mapping/CardTypesMap.cs
@@ -0,0 +1,18 @@
+﻿using FluentNHibernate.Mapping;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApplication.Entity.Mapping
+{
+    class CardTypesMap : SubclassMap<CardTypes>
+    {
+        public CardTypesMap()
+        {
+            Abstract();
+            Map(x => x.CardType).Not.Nullable();
+        }
+    }
+}
