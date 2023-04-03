3bf5b7e15539e984c9ad0890ea1dbce3b9af615c
ITDevSuccess <raharisontsidiany@gmail.com>
2023-04-03 11:53:27 +0300

    SUPPRESSION && REGENERATION DE FRONT

--- Devs/Front/src/app/app.module.ts
+++ Devs/Front/src/app/app.module.ts
@@ -1,21 +1,14 @@
-import { AppRoutingModule } from './app.routing';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './theme/admin/header/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    NavbarComponent,
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    FormsModule,
-    HttpClientModule,
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Devs/Front/src/app/app.routing.ts
+++
@@ -1,6 +0,0 @@
-import { NgModule } from '@angular/core';
-//import { RouterModule } from '@angular/router';
-
-@NgModule()
-
-export class AppRoutingModule {}
