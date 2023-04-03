3439636eb9ba29cb39b82ae2588bf464f641ccc8
ITDevSuccess <raharisontsidiany@gmail.com>
2023-04-03 12:22:12 +0300

    Mise en place de composant inital Header et Footer

--- Devs/Front/src/app/app.module.ts
+++ Devs/Front/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './themes/layout/admin/header/header.component';
+import { FooterComponent } from './themes/layout/admin/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
