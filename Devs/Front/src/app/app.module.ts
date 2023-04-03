75a3f60487a7993ca349d1e08ec68cdfc1feeca5
ITDevSuccess <raharisontsidiany@gmail.com>
2023-04-03 10:56:22 +0300

    Blok

--- Devs/Front/src/app/app.module.ts
+++ Devs/Front/src/app/app.module.ts
@@ -1,21 +1,21 @@
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from './app.routing';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './theme/admin/header/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-
+    AppComponent,
+    NavbarComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpClientModule,
-
-
   ],
   providers: [],
   bootstrap: [AppComponent]
